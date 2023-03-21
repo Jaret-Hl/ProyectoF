10865d8f60261a0f116827dc053dd2e2a1a10b91
Jaret-Hl <gadir041499@gmail.com>
2023-03-21 12:02:49 -0600

    Roles para cada usuario

--- config/app.php
+++ config/app.php
@@ -179,6 +179,7 @@ return [
         App\Providers\FortifyServiceProvider::class,
         App\Providers\JetstreamServiceProvider::class,
     App\Providers\MenuServiceProvider::class,
+    Spatie\Permission\PermissionServiceProvider::class,
 
   ],
 
--- resources/views/content/pages/pages-home.blade.php
+++ resources/views/content/pages/pages-home.blade.php
@@ -8,5 +8,15 @@ $configData = Helper::appClasses();
 
 @section('content')
 <h4>Home Page</h4>
-<h4>Cambios desde local</h4>
+<h4>Este contenido es publico</h4>
+@role('Administrador')
+<h4>Este contenido es para el admin</h4>
+@endrole
+@role('Supervisor')
+<h4>Este contenido es para el supervisor</h4>
+@endrole
+@role('Vendedor')
+<h4>Este contenido es para el vendedor</h4>
+@endrole
+
 @endsection
